7f5f2c36b019d2323e2a7e9e903e7ec3d4e7a3ae
Scott1000 <scott.agua@gmail.com>
2014-01-28 11:25:23 -0800

    add Scaffolf to the app for Aticle and Comment Model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tjbit::Application.config.secret_key_base = '64e7cfd7c52454f04f00675eb853517897b044c52cf3554767918902bfe2d117a0c46f749a7fde2b7b26f3cd51c7ad415cb16a13726a0e63f05db4c8144079d1'
